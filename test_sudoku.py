3ea4b1e069433183ea7f4d07e0b6ea7f5c720940
LCVcode <lcvcode@gmail.com>
2020-06-05 13:55:11 -0400

    Read tests

---
+++ test_sudoku.py
@@ -0,0 +1,16 @@
+import unittest
+from sudoku import Sudoku
+
+class TestSudokuReads(unittest.TestCase):
+    def setUp(self):
+        self.sudoku = Sudoku()
+        for i in range(9):
+            self.sudoku.setAt(i, i, i + 1)
+        self.sudoku.setAt(0, 1, 5)
+    def test_read_row(self):
+        self.assertSetEqual(self.sudoku.readRow(0), {2, 3, 4, 6, 7, 8,9})
+        self.assertSetEqual(self.sudoku.readRow(1), {1, 3, 4, 5, 6, 7, 8, 9})
+        self.assertSetEqual(self.sudoku.readCol(1), {1, 3, 4, 6, 7, 8, 9})
+        self.assertSetEqual(self.sudoku.readBlock(0, 0), self.sudoku.readBlock(1, 1))
+        self.assertSetEqual(self.sudoku.readBlock(0, 0), {4, 6, 7, 8, 9})
+        self.assertSetEqual(self.sudoku.getOptions(2, 1), {4, 6, 7, 8, 9})
