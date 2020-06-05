7f476e9162e6a46bf3803a6fc6bc213deed6c6c5
LCVcode <lcvcode@gmail.com>
2020-06-05 14:14:38 -0400

    Getter, setter, and clear tests

--- test_sudoku.py
+++ test_sudoku.py
@@ -5,6 +5,17 @@ class TestSudokuReads(unittest.TestCase):
     def setUp(self):
         self.sudoku = Sudoku()
         for i in range(9):
+            self.sudoku.grid[i][i] =  i + 1
+        self.sudoku.grid[0][1] = 5
+    def test_set_and_get(self):
+        self.sudoku.clear()
+        for i in range(9):
+            self.assertEqual(self.sudoku.grid[i].count(0), 9)
+        self.sudoku.setAt(5, 5, 5)
+        self.assertEqual(self.sudoku.grid[5][5], 5)
+        self.assertEqual(self.sudoku.getAt(5, 5), 5)
+        self.sudoku.clear()
+        for i in range(9):
             self.sudoku.setAt(i, i, i + 1)
         self.sudoku.setAt(0, 1, 5)
     def test_read_row(self):
