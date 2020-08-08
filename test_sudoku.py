f5124ae33fb8ce8ce55467014ca65cbf10660960
LCVcode <lcvcode@gmail.com>
2020-08-08 13:39:36 -0400

    Swapped unittest for pytest and added it to requirements.txt

--- test_sudoku.py
+++ test_sudoku.py
@@ -1,27 +1,42 @@
-import unittest
+import pytest
+
 from sudoku import Sudoku
 
-class TestSudokuReads(unittest.TestCase):
-    def setUp(self):
-        self.sudoku = Sudoku()
-        for i in range(9):
-            self.sudoku.grid[i][i] =  i + 1
-        self.sudoku.grid[0][1] = 5
-    def test_set_and_get(self):
-        self.sudoku.clear()
-        for i in range(9):
-            self.assertEqual(self.sudoku.grid[i].count(0), 9)
-        self.sudoku.set_at(5, 5, 5)
-        self.assertEqual(self.sudoku.grid[5][5], 5)
-        self.assertEqual(self.sudoku.get_at(5, 5), 5)
-        self.sudoku.clear()
-        for i in range(9):
-            self.sudoku.set_at(i, i, i + 1)
-        self.sudoku.set_at(0, 1, 5)
-    def test_read_row(self):
-        self.assertSetEqual(self.sudoku.read_row(0), {2, 3, 4, 6, 7, 8,9})
-        self.assertSetEqual(self.sudoku.read_row(1), {1, 3, 4, 5, 6, 7, 8, 9})
-        self.assertSetEqual(self.sudoku.read_col(1), {1, 3, 4, 6, 7, 8, 9})
-        self.assertSetEqual(self.sudoku.read_block(0, 0), self.sudoku.read_block(1, 1))
-        self.assertSetEqual(self.sudoku.read_block(0, 0), {4, 6, 7, 8, 9})
-        self.assertSetEqual(self.sudoku.get_options(2, 1), {4, 6, 7, 8, 9})
+@pytest.fixture
+def sudoku():
+    return Sudoku()
+
+def test_sudoku_getters(sudoku):
+    for i in range(9):
+        assert sudoku.get_at(i, i) == 0
+
+def test_sudoku_setters(sudoku):
+    for i in range(9):
+        sudoku.set_at(i, i, i)
+
+        assert sudoku.get_at(i, i) == i
+
+def test_sudoku_read_row(sudoku):
+    sudoku.set_at(0, 0, 2)
+    sudoku.set_at(0, 1, 5)
+    sudoku.set_at(0, 2, 3)
+    sudoku.set_at(0, 3, 1)
+
+    assert sudoku.read_row(0) == {4, 6, 7, 8, 9}
+
+def test_sudoku_read_col(sudoku):
+    sudoku.set_at(1, 0, 4)
+    sudoku.set_at(2, 0, 5)
+    sudoku.set_at(3, 0, 3)
+    sudoku.set_at(4, 0, 1)
+
+    assert sudoku.read_col(0) == {2, 6, 7, 8, 9}
+
+def test_sudoku_read_block(sudoku):
+    sudoku.set_at(3, 3, 1)
+    sudoku.set_at(4, 3, 3)
+    sudoku.set_at(3, 4, 5)
+    sudoku.set_at(5, 5, 9)
+
+    assert sudoku.read_block(3, 3) == {2, 4, 6, 7, 8}
+    assert sudoku.read_block(3, 3) == sudoku.read_block(4, 4)
