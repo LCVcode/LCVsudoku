962f1d7dd457e7aace3b40d00594a2a98309e30c
LCVcode <lcvcode@gmail.com>
2020-08-08 13:59:53 -0400

    Flake8-ification throughout

--- visual.py
+++ visual.py
@@ -1,5 +1,5 @@
 import json
-import sys
+
 
 import pygame as pg
 from sudoku import Sudoku
@@ -13,9 +13,11 @@ with open("visual.json", 'r') as f:
 for key in (k for k in config.keys() if 'border' in config[k].keys()):
     config[key]['border'] = max(0, config[key]['border'])
 
+
 # Build font
 pg.font.init()
 font = pg.font.SysFont(config['font']['name'], 30)
+
 
 def col_locations():
     # Generates left-most coordinate of each column
@@ -24,12 +26,14 @@ def col_locations():
               config['cell']['width']*(i) + \
               config['grid']['border']*2*(i//3 + 1)
 
+
 def row_locations():
     # Generates top-most coordinate of each row
     for i in range(9):
         yield config['cell']['border']*(1+2*i) + \
               config['cell']['height']*(i) + \
               config['grid']['border']*2*(i//3 + 1)
+
 
 def screen_size():
     # Returns screen size as tuple based on visual.json
@@ -39,14 +43,17 @@ def screen_size():
         8*config['grid']['border']
     return (w, h)
 
+
 def get_screen():
     # Returns screen of appropriate size
     screen = pg.display.set_mode(screen_size())
     return screen
 
+
 def json_to_color(json_color):
     # Converts color format in visual.json into a tuple
     return tuple(int(x) for x in json_color.split(','))
+
 
 def draw_sudoku_solve_state(screen, sudoku, box_list=[]):
     # Draws everything on screen with highlights behind cells being solved
@@ -60,12 +67,14 @@ def draw_sudoku_solve_state(screen, sudoku, box_list=[]):
     draw_values(screen=screen, sudoku=sudoku)
     pg.display.flip()
 
+
 def draw_sudoku(screen, sudoku):
     # Draws everything on screen
     screen.fill(json_to_color(config['bg']['color']))
     draw_cells(screen=screen)
     draw_values(screen=screen, sudoku=sudoku)
     pg.display.flip()
+
 
 def draw_cells(screen):
     # Draws all 81 cells on the screen
@@ -74,6 +83,7 @@ def draw_cells(screen):
     for x in row_locations():
         for y in col_locations():
             pg.draw.rect(screen, color, pg.Rect(y, x, width, height))
+
 
 def draw_values(screen, sudoku):
     # Draws cell text on the screen
@@ -85,18 +95,23 @@ def draw_values(screen, sudoku):
                 cell_text = font.render(str(n), False, color)
                 screen.blit(cell_text, (y, x))
 
+
 def draw_box(screen, x, y, color):
     # Draws highlights behind cells to show solve progress
     border = config['cell']['border']
     x = tuple(row_locations())[x] - border
     y = tuple(col_locations())[y] - border
     border *= 2
-    box = pg.Rect(y, x, config['cell']['width']+border, config['cell']['height']+border)
+    wid = config['cell']['width']+border
+    hei = config['cell']['height']+border
+    box = pg.Rect(y, x, wid, hei)
     pg.draw.rect(screen, color, box)
+
 
 if __name__ == '__main__':
     visual = get_screen()
     board = Sudoku(40)
-    draw_sudoku_solve_state(visual, board, (((0, 0), 'highlight'), ((0, 1), 'rollback')))
+    highlights = (((0, 0), 'highlight'), ((0, 1), 'rollback'))
+    draw_sudoku_solve_state(visual, board, highlights)
     import time
     time.sleep(3)
